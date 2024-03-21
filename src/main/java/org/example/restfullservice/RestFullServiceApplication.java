6bb8761e6cbff433a2e9d0312eb075131d4126f6
Makar147 <mmakabaev@gmail.com>
2024-03-22 00:42:45 +0500

    first commit

---
+++ src/main/java/org/example/restfullservice/RestFullServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.restfullservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/restfullservice/RestFullServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restfullservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
